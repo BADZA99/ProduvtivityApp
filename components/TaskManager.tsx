6ac73b6f979dcd5143ef4d0a14ac662254673cfb
papabn <pndiaye999@gmail.com>
2025-08-22 00:08:25 +0000

    refactor: simplify HomeScreen layout and integrate TaskManager component feat: add react-native-calendars and react-native-vector-icons dependencies

--- components/TaskManager.tsx
+++ components/TaskManager.tsx
@@ -1,5 +1,18 @@
+import { View,Text,TextInput,TouchableOpacity,FlatList,KeyboardAvoidingView,Platform,Modal } from 'react-native';
+import { useState } from 'react';
+import { Calendar } from 'react-native-calendars';
 
-import { View } from 'react-native'
+type TaskStatus = 'not started' |'in progress' | 'completed';
+
+type Task = {
+  id: string;
+  title: string;
+  description: string;
+  status: TaskStatus;
+  dueDate: string;
+};
+
+
 
 export default function TaskManager() {
   return (
